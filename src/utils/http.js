01f1ce3a6dc1641be33dd3cb6e7fb95d73c08f9b
CXQZTMDS666 <a1875051871@outlook.com>
2025-11-25 02:16:08 +0800

    axios基础封装

---
+++ src/utils/http.js
@@ -0,0 +1,21 @@
+//axios基础的封装
+import axios from "axios";
+
+const httpInstance = axios.create({
+    baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+    timeout:5000
+})
+
+//拦截器
+
+//请求拦截
+httpInstance.interceptors.request.use(config =>{
+    return config
+},e => Promise.reject(e))
+
+//响应拦截
+httpInstance.interceptors.response.use(res => res.data,e => {
+    return Promise.reject(e)
+})
+
+export default httpInstance
