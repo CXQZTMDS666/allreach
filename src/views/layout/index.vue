9d2478d8a8f7b5be00d556678d746d13a4471a0d
CXQZTMDS666 <a1875051871@outlook.com>
2025-11-25 21:02:56 +0800

    请求次数优化

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -3,6 +3,15 @@
   import layoutHeader from './components/layoutHeader.vue'
   import layoutFooter from './components/layoutFooter.vue'
   import layoutFixed from './components/layoutFixed.vue'
+
+//触发获取导航列表的action
+
+import { useCategoryStore } from '@/stores/category'
+import { onMounted } from 'vue'
+
+const categoryStore = useCategoryStore()
+
+onMounted(() => categoryStore.getCategory())
 </script>
 
 <template>
